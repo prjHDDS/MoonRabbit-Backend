f93058a7654ad14a15b2b3984a8f8b7b37241f88
vloeiolzlr <yoonseed1995@naver.com>
2025-03-24 23:57:00 +0900

    refactor: 에러처리기능 병합

--- src/main/java/com/bigpicture/moonrabbit/global/exception/UserAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.bigpicture.moonrabbit.global.exception;
-
-public class UserAlreadyExistsException extends CustomException {
-    public UserAlreadyExistsException() {
-        super(ErrorCode.USER_ALREADY_EXISTS);
-    }
-}
