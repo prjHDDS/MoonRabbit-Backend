738e3a1a33095f7ce7dd6040019a7829003216f0
김동민 <68054919+Vloeiolzlr@users.noreply.github.com>
2025-03-27 00:40:42 +0900

    [feat] 카카오 사용자 DB 저장 (#13)
    * feat: 카카오 사용자 정보 DB 저장
    
    * fix: 도메인주소 localhost로 변경
    
    ---------
    
    Co-authored-by: HYEON <74049556+titeotty@users.noreply.github.com>

---
+++ src/main/java/com/bigpicture/moonrabbit/global/auth/kakao/KakaoUserResponseDTO.java
@@ -0,0 +1,20 @@
+package com.bigpicture.moonrabbit.global.auth.kakao;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class KakaoUserResponseDTO {
+    private Long id;
+    private String email;
+    private String nickname;
+    private String profileImageUrl;
+
+    public KakaoUserResponseDTO(Long id, String email, String nickname, String profileImageUrl) {
+        this.id = id;
+        this.email = email;
+        this.nickname = nickname;
+        this.profileImageUrl = profileImageUrl;
+    }
+}
