3c1867cd4380b459eb2f07cc062c0a3d65489d30
김동민 <68054919+Vloeiolzlr@users.noreply.github.com>
2025-03-26 14:43:21 +0900

    #9 [refactor] 에러 처리 병합
    * [build] 기본적인 패키지 세팅
    
    * chore: 패키지 도메인 변환 작업
    
    * chore: 패키지 도메인 변환 작업
    
    * remove: 미사용 파일 제거
    
    * refactor: 에러처리기능 병합
    
    ---------
    
    Co-authored-by: HYEON <74049556+titeotty@users.noreply.github.com>

--- src/main/java/com/bigpicture/moonrabbit/domain/user/service/UserService.java
+++ src/main/java/com/bigpicture/moonrabbit/domain/user/service/UserService.java
@@ -1,11 +1,13 @@
 package com.bigpicture.moonrabbit.domain.user.service;
 
 import com.bigpicture.moonrabbit.domain.user.entity.User;
-import com.bigpicture.moonrabbit.global.exception.UserAlreadyExistsException;
-import com.bigpicture.moonrabbit.global.exception.UserNotFoundException;
+//import com.bigpicture.moonrabbit.global.exception.UserAlreadyExistsException;
+//import com.bigpicture.moonrabbit.global.exception.UserNotFoundException;
 import com.bigpicture.moonrabbit.domain.user.repository.UserRepository;
+import com.bigpicture.moonrabbit.global.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import com.bigpicture.moonrabbit.global.exception.CustomException;
 
 import java.util.Optional;
 
@@ -18,7 +20,7 @@ public class UserService {
     public User saveUser(User user) {
         Optional<User> existingUser = userRepository.findByEmail(user.getEmail());
         if (existingUser.isPresent()) {
-            throw new UserAlreadyExistsException();
+            CustomException.UserAlreadyExistsException();
         }
         return userRepository.save(user);
     }
@@ -26,6 +28,6 @@ public class UserService {
     // 이메일로 유저 찾기
     public User getUserByEmail(String email) {
         return userRepository.findByEmail(email)
-                .orElseThrow(UserNotFoundException::new);
+                .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
     }
 }
